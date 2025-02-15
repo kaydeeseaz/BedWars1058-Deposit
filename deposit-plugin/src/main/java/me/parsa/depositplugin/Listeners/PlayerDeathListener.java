62625b1ea3fdc34f0ddb2c81b536616af574b749
parsa3323 <mehr2.business@gmail.com>
2025-02-15 00:43:43 -0800

    Dev (#1006) Version 1.4.6 B E S T P Ls

--- deposit-plugin/src/main/java/me/parsa/depositplugin/Listeners/PlayerDeathListener.java
+++ deposit-plugin/src/main/java/me/parsa/depositplugin/Listeners/PlayerDeathListener.java
@@ -14,12 +14,17 @@ public class PlayerDeathListener implements Listener {
     @EventHandler
     public void onDeath(PlayerKillEvent event){
         if (DepositPlugin.plugin.getConfig().getBoolean("disable-hologram-after-death")) {
+            DepositPlugin.debug("PlayerKillEvent triggered");
             new BukkitRunnable() {
                 @Override
                 public void run() {
+                    DepositPlugin.debug("Run for player death triggered");
                     Player player = event.getVictim();
+                    DepositPlugin.debug("Initializing gamestartlistener");
                     GameStartListener gameStartListener = new GameStartListener(DepositPlugin.plugin, ArenasConfig.get());
+                    DepositPlugin.debug("Deleting hd");
                     gameStartListener.deleteHolograms(player);
+                    DepositPlugin.debug("Done");
                 }
             }.runTaskLaterAsynchronously(DepositPlugin.plugin, 5L);
 
